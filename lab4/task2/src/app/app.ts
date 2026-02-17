eb2282d32ea110a30ccd9c87be927b1b2e36ce96
Karshyga07 <karshygaburkitbay3@gmail.com>
2026-02-17 13:38:12 +0500

    add

--- lab4/task2/src/app/app.ts
+++ lab4/task2/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterModule,ProductListComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('task2');
+export class AppComponent { 
+  title = 'online-store';
 }
